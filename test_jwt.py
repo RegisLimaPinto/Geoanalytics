8031812e4384318790e4853d4f19a9a1b7157ec6
RegisLimaPinto <regislimapinto@gmail.com>
2026-05-02 13:37:24 -0300

    chore: remover arquivos de teste e notebook temp

--- test_jwt.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '/app')
-from app.auth import create_access_token, decode_token
-tok = create_access_token({'sub': 1})
-print('TOKEN_PREFIX:', tok[:40])
-d = decode_token(tok)
-print('DECODED:', d)
-print('AUTH_OK:', d is not None and d.get('sub') == 1)
